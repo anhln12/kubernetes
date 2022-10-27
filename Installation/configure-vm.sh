219b02bee8dfe1ea9da93069c9ae18d9d7d6f38e
Le Anh <anhle0412@gmail.com>
2022-10-27 15:53:46 +0700

    Create configure-vm.sh

---
+++ Installation/configure-vm.sh
@@ -0,0 +1,18 @@
+#install some packages
+sudo yum update -y
+sudo yum install telnet -y
+sudo yum install bind-utils -y
+sudo yum install net-tools -y
+sudo yum install bash-completion bash-completion-extras
+source /etc/profile.d/bash_completion.sh
+
+#Disable SELinux
+sudo setenforce 0
+sudo sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+#Disable firewall
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+
+#Enable IP Forwarding
+sudo sysctl -w net.ipv4.ip_forward=1
